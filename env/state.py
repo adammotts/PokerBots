f42cbaa17b6aa7e547b55bd8cc6cb0d8670c465d
Adam Ma <adam.zym@gmail.com>
2026-03-25 16:18:40 -0400

    Old Man Coffee

--- env/state.py
+++ env/state.py
@@ -2,12 +2,19 @@ from collections import OrderedDict
 
 import numpy as np
 import numpy.typing as npt
+from rlcard.games.base import Card
 
 
 class State:
     def __init__(
-        self, *, obs: npt.NDArray[np.float64], legal_actions: OrderedDict[int, None], player_id: int,
+        self,
+        *,
+        obs: npt.NDArray[np.float64],
+        legal_actions: OrderedDict[int, None],
+        player_id: int,
+        hand: tuple[Card, Card],
     ) -> None:
         self.obs: npt.NDArray[np.float64] = obs
         self.legal_actions: OrderedDict[int, None] = legal_actions
         self.player_id: int = player_id
+        self.hand: tuple[Card, Card] = hand
---
+++ players/old_man_coffee_player.py
@@ -0,0 +1,31 @@
+from env.action import Action
+from env.state import State
+from players.base_player import BasePlayer
+
+
+class OldManCoffeePlayer(BasePlayer):
+    def __init__(self) -> None:
+        pass
+
+    def act(self, state: State) -> int:
+        c1, c2 = state.hand
+
+        if (
+            c1.rank == c2.rank == "A"
+            or c1.rank == c2.rank == "K"
+            or c1.rank == c2.rank == "Q"
+        ):
+            if Action.RAISE.value in state.legal_actions:
+                return Action.RAISE.value
+
+            else:
+                return Action.CALL.value
+
+        elif Action.CHECK.value in state.legal_actions:
+            return Action.CHECK.value
+
+        else:
+            return Action.FOLD.value
+
+    def reset(self) -> None:
+        pass
